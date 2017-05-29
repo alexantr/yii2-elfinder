13b6dd24269976ba06a0e0691c08b94771d32bf8
Alex Yashkin <alex.yashkin@gmail.com>
2017-05-29 12:00:16 +0300

    Added compatibility with phpunit 6

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,5 +9,12 @@ define('YII_DEBUG', true);
 require_once(__DIR__ . '/../vendor/autoload.php');
 require_once(__DIR__ . '/../vendor/yiisoft/yii2/Yii.php');
 
+// backward compatibility for php 5.5 and low
+if (!class_exists('PHPUnit_Framework_TestCase') && class_exists('PHPUnit\Framework\TestCase')) {
+    abstract class PHPUnit_Framework_TestCase extends PHPUnit\Framework\TestCase
+    {
+    }
+}
+
 Yii::setAlias('@tests', __DIR__);
 Yii::setAlias('@alexantr/elfinder', dirname(__DIR__) . '/src');
